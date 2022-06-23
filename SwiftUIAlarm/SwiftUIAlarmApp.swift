c4a7800215b9b9a254fcf2b38c48aa883feb56ca
MJ <mj.mu@bionime.com>
2022-06-23 13:30:07 +0800

    Initial Commit

---
+++ SwiftUIAlarm/SwiftUIAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlarmApp.swift
+//  SwiftUIAlarm
+//
+//  Created by 辴 穆 on 2022/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
